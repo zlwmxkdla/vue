7b6a177a325571e3fc9bd1f222f8b562fbff6941
zlwmxkdla <zlwmxkdla@naver.com>
2025-03-23 19:57:08 +0900

    inital commit

---
+++ test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <li>
+            <input type="checkbox" v-model="checked"/>옵션1
+        </li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"CheckboxItem",
+        data(){
+            return{
+                checked:false
+            };
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
